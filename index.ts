68de127d558ef452e3b9b762dc9b72ec4da2f9b2
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-03-14 18:00:40 -0400

    fix api

--- index.ts
+++ index.ts
@@ -20,12 +20,15 @@ app.use(cors({
     credentials: true
 }));
 
-app.options("*", (req, res) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.sendStatus(204);
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://sentient-app.netlify.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  next();
 });
 
 // Middleware
