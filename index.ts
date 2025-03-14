e550614481ae88b56eb8b355f1ae22ea302448d3
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-03-14 17:17:50 -0400

    fix api

--- index.ts
+++ index.ts
@@ -20,6 +20,14 @@ app.use(cors({
     credentials: true
 }));
 
+app.options("*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.sendStatus(204);
+});
+
 // Middleware
 app.use(compression());
 app.use(bodyParser.json());
