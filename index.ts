41296fc6b880e69fe61a5cb95f711aadc9e2b825
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-03-14 17:48:31 -0400

    fix api

--- index.ts
+++ index.ts
@@ -17,6 +17,7 @@ app.use(cors({
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Authorization"],
     credentials: true
 }));
 
