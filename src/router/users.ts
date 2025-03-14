38dfe9e11e22b13b82ba6f451a4eed9d3e0b729d
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-03-14 17:04:09 -0400

    fix api

--- src/router/users.ts
+++ src/router/users.ts
@@ -4,17 +4,17 @@ import {isAuthenticated, isOwner} from "../middlewares";
 
 export default (router:express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
-    router.get("/test-db", async (req, res) => {
-        console.log("Testing database connection...");
+    router.get("/test-db", async (_req, res) => {
+      console.log("Testing database connection...");
 
-        try {
-            const users = await User.find().limit(1); // Fetch one user
-            console.log("MongoDB connection successful!");
-            return res.json({ success: true, user: users[0] || "No users found" });
-        } catch (error) {
-            console.error("MongoDB Connection Error:", error);
-            return res.status(500).json({ error: "Database connection failed" });
-        }
+      try {
+        const users = getAllUsers;
+        console.log("MongoDB connection successful!");
+        return res.json({ success: true, user: users || "No users found" });
+      } catch (error) {
+        console.error("MongoDB Connection Error:", error);
+        return res.status(500).json({ error: "Database connection failed" });
+      }
     });
     router.delete('/users/:id', isOwner, isAuthenticated, deleteUser);
     router.patch('/users/:id', isOwner, isAuthenticated, updateUser);
