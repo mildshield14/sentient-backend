e3e675bbbd409ac7eadbfdbfade4557b37af8ddf
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-03-18 11:37:34 -0400

    fix weather api

--- src/router/openweather.ts
+++ src/router/openweather.ts
@@ -10,7 +10,7 @@ export default (router: express.Router) => {
 
     axios
       .get(
-        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_API_KEY}`,
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_API_KEY}`,
       )
       .then((data: { data: any }) => {
         res.json(data.data);
