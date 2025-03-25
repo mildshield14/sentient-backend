feba43c335d8cc0720caf421c71ed57bb9031d71
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-03-25 12:09:04 -0400

    change to test mode spotify

--- src/router/spotify.ts
+++ src/router/spotify.ts
@@ -4,7 +4,7 @@ import SpotifyWebApi from 'spotify-web-api-node';
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: 'https://sentient-backend-aphg-pied.vercel.app/api/callback',
+    redirectUri: 'http://localhost:5173/callback', // TODO: Change this to netlifyy domain
 });
 
 export default (router:express.Router) => {
